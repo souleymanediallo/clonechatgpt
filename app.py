cb65b63563c89cd62fe8f32c303f46e4aedea896
souleymanediallo <souleymanedialloo@yahoo.fr>
2023-12-07 20:42:13 +0100

    build conversation

--- app.py
+++ app.py
@@ -16,5 +16,24 @@ def hello_world():
     return render_template('pages/index.html')
 
 
+def build_conversation(messages: list) -> list[dict]:
+    return [
+        {"role": "user" if i % 2 == 0 else "assistant", "content": message}
+        for i, message in enumerate(messages)
+    ]
+
+
+def event_stream(conversation: list[dict]):
+    response = openai.Completion.create(
+        model="gpt-3.5-turbo",
+        messages=conversation,
+        stream=True
+    )
+    for line in response:
+        text = line.choices[0].delta.get("content", "")
+        if text:
+            yield text
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
