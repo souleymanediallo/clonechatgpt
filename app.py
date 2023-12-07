5127d5aa722c4a0e8520b044fada5a4f4fd3aa01
souleymanediallo <souleymanedialloo@yahoo.fr>
2023-12-07 20:11:39 +0100

    add openai_api_key

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
 
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 app = Flask(__name__)
 
 
