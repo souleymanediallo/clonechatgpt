cb84a23abcbbc8ca22bec306e215e217a7d17176
souleymanediallo <souleymanedialloo@yahoo.fr>
2023-12-08 23:51:12 +0100

    functions javascript

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
 
 import os
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"),)
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('pages/index.html')
 
 
-def build_conversation(messages: list) -> list[dict]:
+@app.route('/prompt', methods=['POST'])
+def prompt():
+    messages = request.json['messages']
+    conversation = build_conversation_dict(messages=messages)
+    return Response(event_stream(conversation), mimetype='text/event-stream')
+
+
+def build_conversation_dict(messages: list) -> list[dict]:
     return [
         {"role": "user" if i % 2 == 0 else "assistant", "content": message}
         for i, message in enumerate(messages)
@@ -24,13 +31,13 @@ def build_conversation(messages: list) -> list[dict]:
 
 
 def event_stream(conversation: list[dict]):
-    response = openai.Completion.create(
+    response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=conversation,
         stream=True
     )
     for line in response:
-        text = line.choices[0].delta.get("content", "")
+        text = getattr(line.choices[0].delta, "content", "")
         if text:
             yield text
 
